ecd6fe844aee68dac62f1d5d140f74ebdb7c7ec5
samarthmore078 <samarthmore369@gmail.com>
2025-03-26 20:34:24 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import './App.css'
+import Login from './components/Auth/Login'
+import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
+
+function App() {
+  return(
+  <>
+     {/* <Login /> */}
+     <EmployeeDashboard />
+
+    </>
+  )
+  
+}
+
+export default App
